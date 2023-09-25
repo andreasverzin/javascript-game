16f7a63e31456140f045649e92122636e1435c87
andreasverzin <Artecovec@gmail.com>
2023-09-25 17:09:55 +0400

    added animation

--- script.js
+++ script.js
@@ -60,9 +60,13 @@ window.addEventListener('load', function() {
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
             this.speedY = 0;
-            this.maxSpeed = 3;
+            this.maxFrame = 37;
+            this.maxSpeed = 7;
             this.projectiles = [];
+            this.image = document.getElementById('player');
         }
 
         update() {
@@ -75,13 +79,20 @@ window.addEventListener('load', function() {
             this.projectiles.forEach(projectile => {
                 projectile.update();
             });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
 
-            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion)
+            //sprite animation
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            }
         }
 
         draw(context) {
             context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
@@ -130,11 +141,48 @@ window.addEventListener('load', function() {
     }
 
     class Layer {
+        constructor(game, image, speedModifier) {
+            this.game = game;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.width = 1768;
+            this.height = 500;
+            this.x = 0;
+            this.y = 0;
+        }
 
+        update() {
+            if (this.x <= -this.width) this.x = 0;
+            this.x -= this.game.speed * this.speedModifier;
+        }
+
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y);
+            context.drawImage(this.image, this.x + this.width, this.y);
+        }
     }
 
     class Background {
+        constructor(game) {
+            this.game = game;
+            this.image1 = document.getElementById('layer1');
+            this.image2 = document.getElementById('layer2');
+            this.image3 = document.getElementById('layer3');
+            this.image4 = document.getElementById('layer4');
+            this.layer1 = new Layer(this.game, this.image1, 0.2);
+            this.layer2 = new Layer(this.game, this.image2, 0.4);
+            this.layer3 = new Layer(this.game, this.image3, 1);
+            this.layer4 = new Layer(this.game, this.image4, 1.3);
+            this.layers = [this.layer1, this.layer2, this.layer3];
+        }
 
+        update() {
+            this.layers.forEach(layer => layer.update());
+        }
+
+        draw(context) {
+            this.layers.forEach(layer => layer.draw(context));
+        }
     }
 
     class UI {
@@ -186,6 +234,7 @@ window.addEventListener('load', function() {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler(this);
             this.ui = new UI(this);
@@ -202,11 +251,14 @@ window.addEventListener('load', function() {
             this.winningScore = 10;
             this.gameTime = 0;
             this.timeLimit = 5000;
+            this.speed = 1;
         }
 
         update(deltaTime) {
             if (!this.gameOver) this.gameTime += deltaTime;
-            if (this.gameTime > this.timeLimit) this.gameOver = true; 
+            if (this.gameTime > this.timeLimit) this.gameOver = true;
+            this.background.update();
+            this.background.layer4.update(); 
             this.player.update();
             if (this.ammoTimer > this.ammoInterval) {
                if (this.ammo < this.maxAmmo) this.ammo++;
@@ -241,11 +293,13 @@ window.addEventListener('load', function() {
         }
 
         draw(context) {
+            this.background.draw(context);
             this.player.draw(context);
             this.ui.draw(context);
             this.enemies.forEach(enemy => {
                 enemy.draw(context);
             });
+            this.background.layer4.draw(context);
         }
 
         addEnemy() {
